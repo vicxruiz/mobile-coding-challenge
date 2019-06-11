17f85e456d5ad5e04ef96ae025561bae62b5d6c5
victor <ruizdvictor@gmail.com>
2019-06-11 16:10:14 -0400

    added logic to check for repo data in each cell and update views

--- GithubRepo/Views/RepositoryTableViewCell.swift
+++ GithubRepo/Views/RepositoryTableViewCell.swift
@@ -10,9 +10,32 @@ import Foundation
 import UIKit
 
 class RepositoryTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var repoTitleLabel: UILabel!
     @IBOutlet weak var repoDescriptionLabel: UILabel!
     @IBOutlet weak var repoStarCountLable: UILabel!
+    
+    //Checking for data
+    var dataGetter: DataGetter?
+    var repository: Repository? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        //unwrap object
+        guard let repository = repository else {
+            print("no song")
+            return
+        }
+        //setting cell labels to object properties
+        repoTitleLabel.text = repository.name
+        repoDescriptionLabel.text = repository.description
+        repoStarCountLable.text = "\(repository.stargazers_count)"
+    }
+    
 }
